a7e8482be9f013ef5388644f97d54695def3c338
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-11-16 11:01:21 +0100

    Configure model and views

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,16 @@
 class Group < ApplicationRecord
   belongs_to :user
-  has_many :groups
+  has_many :entities
+
+  has_one_attached :icon
 
   validates :name, :icon, presence: true
+
+  def total_amount
+    total = 0
+    entities.each do |ent|
+        total += ent.amount
+    end
+    total
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, sign_out_via: [:get]
   resources :groups
   resources :entities
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "groups#index"
 end
