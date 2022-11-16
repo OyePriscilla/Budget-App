91fb2f3a978d56b5ee13db856cbb1519eb2cac3a
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-11-16 17:55:43 +0100

    update controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, sign_out_via: [:get]
-  resources :groups
-  resources :entities
+  resources :groups, only: [:index, :new, :create] do
+    resources :entities, only: [:index, :new, :create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
