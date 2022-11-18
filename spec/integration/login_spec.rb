fa00f22cfe8f00ed1cbf090b2e14146a977cea75
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-11-18 16:47:59 +0100

    Fix linters offenses

--- spec/integration/login_spec.rb
+++ spec/integration/login_spec.rb
@@ -5,16 +5,16 @@ RSpec.describe 'Log in page', type: :feature do
     @user = User.new(name: 'AdeJesu', email: 'ade@ade.com', password: 'AdeOye')
 
     visit new_user_session_path
-      fill_in 'Email', with: 'ade@ade.com'
-      fill_in 'Password', with: 'AdeOye'
-      click_button 'Log in'
-      visit groups_path
+    fill_in 'Email', with: 'ade@ade.com'
+    fill_in 'Password', with: 'AdeOye'
+    click_button 'Log in'
+    visit groups_path
   end
 
   describe 'integration' do
     before(:each) { visit new_user_session_path(@user) }
 
-      scenario 'displays content' do
+    scenario 'displays content' do
       expect(page).to have_content 'Log in'
     end
 
@@ -28,7 +28,7 @@ RSpec.describe 'Log in page', type: :feature do
     end
 
     scenario 'displays email' do
-      expect(page).to have_content("Email")
+      expect(page).to have_content('Email')
     end
 
     scenario 'displays pagination' do
